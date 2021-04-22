b9c66ac12d3bc94f9fbd1822290c5c0965dc6790
sadaf <sadaf17attar@gmail.com>
2021-04-22 06:27:01 +0200

    Clone file committed

--- demoBrowser.py
+++ demoBrowser.py
@@ -11,8 +11,10 @@ print(driver.title)
 print(driver.current_url)
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.minimize_window()
-driver.back()
 driver.refresh()
+driver.refresh()
+driver.back()
+#driver.refresh()
 driver.close()
 
 
